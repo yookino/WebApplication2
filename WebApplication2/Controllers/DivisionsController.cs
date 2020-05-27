d09cc7ffb6b97d97a375c3d8614b849b1febb8a3
Itthipon Treetodbadee <yookino@gmail.com>
2020-05-27 19:11:09 +0700

    Add project files.

---
+++ WebApplication2/Controllers/DivisionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class DivisionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TestContext context = new TestContext();
+
+            // Select * FROM Divisions sql command
+            var divisions = context.Divisions.ToList(); //Entity Framework
+
+            return Json(divisions);
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Divisions divisions)
+        {
+            TestContext context = new TestContext();
+            context.Divisions.Add(divisions);
+            context.SaveChanges();
+
+            return Json(divisions);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class PersonController : Controller
+    {
+        // fucntion, method, action
+        public string Index(int id = 0)
+        {
+            return "Hello World." + id;
+        }
+
+        // action Hello
+        public string Hello(string name = "")
+        {
+            return "Hello " + name;
+        }
+
+        public IActionResult Person()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+    }
+}
